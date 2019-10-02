63b17fe10f477ed35f16f05b1dcb1d07889103f0
Curt Miller <curtdmiller@gmail.com>
2019-10-02 11:05:58 -0400

    complete first book cover: Albers Interaction

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var browserSync = require( 'browser-sync' ).create();
+
+function styles() {
+	return gulp.src( './styles/**/*.scss' )
+    .pipe( sass().on( 'error', sass.logError ) )
+    .pipe( gulp.dest( 'styles/') )
+		.pipe( browserSync.stream() );
+}
+
+function reload(done) {
+	browserSync.reload();
+	done();
+}
+
+function serve() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+
+	gulp.watch( './index.html', gulp.series(reload) );
+	gulp.watch( './styles/**/*.scss', gulp.series(styles) );
+	// gulp.watch( './js/*.js', gulp.series(reload) );
+}
+
+exports.reload = reload;
+exports.styles = styles;
+exports.serve = serve;
+
+exports.default = serve;
