19dc3430c6f0945af8d963ceed75ac51b32690c4
Curt Miller <curtdmiller@gmail.com>
2019-10-11 19:51:08 -0400

    complete Josef Albers, Pillars. update js to use aot class

--- scripts.js
+++ scripts.js
@@ -1,40 +1,15 @@
 (function(){
 
-var interaction = document.getElementsByClassName( 'albers-interaction' )[0];
-var color = document.getElementsByClassName( 'albers-color' )[0];
-var perc = document.getElementsByClassName( 'percussion' )[0];
-var perciii = document.getElementsByClassName( 'percussion-vol3' )[0];
+// get animate on touch containers
+var aot = document.getElementsByClassName( 'aot' );
 
 document.body.addEventListener( 'touchend', function( event ){
-  if ( interaction.contains( event.target ) ) {
-    interaction.classList.toggle( 'touched' );
-
-    color.classList.remove( 'touched' );
-    perc.classList.remove( 'touched' );
-    perciii.classList.remove( 'touched' );
-  } else if ( color.contains( event.target ) ){
-    color.classList.toggle( 'touched' );
-
-    interaction.classList.remove( 'touched' );
-    perc.classList.remove( 'touched' );
-    perciii.classList.remove( 'touched' );
-  } else if ( perc.contains( event.target ) ){
-    perc.classList.toggle( 'touched' );
-
-    color.classList.remove( 'touched' );
-    interaction.classList.remove( 'touched' );
-    perciii.classList.remove( 'touched' );
-  } else if ( perciii.contains( event.target ) ){
-    perciii.classList.toggle( 'touched' );
-
-    color.classList.remove( 'touched' );
-    interaction.classList.remove( 'touched' );
-    perc.classList.remove( 'touched' );
-  } else {
-    interaction.classList.remove( 'touched' );
-    color.classList.remove( 'touched' );
-    perc.classList.remove( 'touched' );
-    perciii.classList.remove( 'touched' );
+  for (var i = 0; i < aot.length; i++) {
+    if ( aot[i].contains( event.target) ) {
+      aot[i].classList.toggle( 'touched' );
+    } else {
+      aot[i].classList.remove( 'touched' );
+    }
   }
 }, false)
 
